286ad7ea6e66d7be46cce50761c61db0a5f7f588
telosma <duylxbk57@gmail.com>
2016-10-21 15:47:19 +0700

    Add some images

---
+++ config/asset.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'btn_gohome' => asset('images/default/btn-gohome.png'),
+    'default_avatar' => asset('images/default/avatar.png'),
+    'not_found' => asset('images/default/404.png'),
+    'logo_fr' => asset('images/default/logo-fr.png'),
+    'search_icon' => asset('images/default/searchicon.png'),
+    'active_state' => asset('images/default/active-state.png'),
+];
