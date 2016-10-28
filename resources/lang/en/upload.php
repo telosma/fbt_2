faf0d3fb2adde00b0c7d4e226c2fbe3b6a4b3b8c
Hoang Hoi <hoanghoi1310@gmail.com>
2016-10-28 21:16:12 +0700

    Upload multiple file

---
+++ resources/lang/en/upload.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'add' => 'Add',
+    'remove_all' => 'Remove all',
+    'upload' => 'Upload',
+    'stop' => 'Stop',
+    'finish' => 'Upload finish!',
+    'stop_upload' => 'Upload stopped!',
+    'file_big' => ' too big',
+];
